bbe44052ae015d22a9092a1b24f7fec2c8a43323
okietokie <kalyanisanthoshkumar@gmail.com>
2026-02-14 09:54:55 +0400

    Switch axios baseURL to use environment variable

--- student-council-frontend/src/api/axiosClient.js
+++ student-council-frontend/src/api/axiosClient.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  //baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`, 
-  baseURL: `http://localhost:5000/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`, 
+  //baseURL: `http://localhost:5000/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
